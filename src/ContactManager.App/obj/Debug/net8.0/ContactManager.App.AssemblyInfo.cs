6f9b39d9ca0183590ec1b120b5c3b06db5327415
Michael Hoertenhuber <m.hoertenhuber2000@gmail.com>
2025-12-09 18:49:22 +0100

    No changes

--- src/ContactManager.App/obj/Debug/net8.0/ContactManager.App.AssemblyInfo.cs
+++ src/ContactManager.App/obj/Debug/net8.0/ContactManager.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactManager.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586902b4f473da81824daa766f4d5e55f527854c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6115f6ab82d7c31e12f7c7f787077f276509cdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactManager.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactManager.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
